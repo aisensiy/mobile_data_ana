a7192f3d78008ab35052331174fd0800fe78cc58
aisensiy <aisensiy@gmail.com>
2014-12-02 10:01:49 +0800

    add costs and log for clean data

--- clean_data_split_by_user_id.py
+++ clean_data_split_by_user_id.py
@@ -7,10 +7,20 @@ import glob
 import csv
 from common import create_dir_if_not_exists
 from constants import log_header as raw_log_header
+from datetime import datetime
 
 
 logging.basicConfig(format='%(asctime)s-[%(levelname)s]: %(message)s',
                     level=logging.DEBUG)
+
+start_at = datetime.now()
+
+
+def costs():
+    delta = datetime.now() - start_at
+    return '%02d:%02d:%02d' % (delta.seconds / 3600,
+                               delta.seconds % 3600 / 60,
+                               delta.seconds % 60)
 
 
 def clean_data(filepath, dst_filepath, header):
@@ -48,15 +58,22 @@ def clean_data(filepath, dst_filepath, header):
         logging.debug('remove na rows: %d' % (origin_len - cleaned_len))
 
 
-def main(src_dir, dst_dir):
+def main(src_dir, dst_dir, valid_user_file):
     create_dir_if_not_exists(dst_dir)
     csv_files = glob.glob(os.path.join(src_dir, '*', '*.csv'))
-    for csv_file in csv_files:
+    total_file_cnt = len(csv_files)
+    valid_users = set(map(lambda x: x.strip(),
+                          open(valid_user_file).readlines()))
+    for cnt, csv_file in enumerate(csv_files):
+        logging.info('[%d/%d] processing: %s costs: %s',
+                     cnt + 1, total_file_cnt, csv_file, costs())
         filename = os.path.basename(csv_file)
         dirname = os.path.splitext(filename)[0][-2:]
+        uid = os.path.splitext(filename)[0]
+        if uid not in valid_users:
+            continue
         create_dir_if_not_exists(os.path.join(dst_dir, dirname))
         dst_csv_file = os.path.join(dst_dir, dirname, filename)
-        logging.info('clean data from %s to %s', csv_file, dst_csv_file)
         clean_data(csv_file, dst_csv_file, raw_log_header)
 
 
@@ -64,4 +81,4 @@ if __name__ == '__main__':
     import sys
     src_dir = sys.argv[1]
     dst_dir = sys.argv[2]
-    main(src_dir, dst_dir)
+    main(src_dir, dst_dir, 'valid_users.csv')
