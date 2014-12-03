e7f85498a7017149945b4b32eeead7882b200a70
aisensiy <aisensiy@gmail.com>
2014-12-03 14:47:39 +0800

    fetch base statiion info

--- get_address_and_pois_for_base_station.py
+++ get_address_and_pois_for_base_station.py
@@ -8,6 +8,7 @@ Output: file with json data fetch from map.baidu.api
 
 import requests
 import csv
+import time
 from constants import BAIDU_MAP_AK
 import logging
 
@@ -15,30 +16,46 @@ logging.basicConfig(format='%(asctime)s-[%(levelname)s]: %(message)s',
                     level=logging.INFO)
 
 
+def cal_offset(latitude, longitude):
+    return float(latitude) + 0.007, float(longitude) + 0.013
+
+
 def fetch_json_data(url):
-    r = requests.get(url)
-    return r.text
+    try:
+        r = requests.get(url, timeout=10)
+    except:
+        r = None
+
+    return r.text if r else None
 
 
 def generate_url(latitude, longitude):
-    return 'http://api.map.baidu.com/geocoder/v2/?ak=%s&location=%s,%s&output=json&pois=1' % (BAIDU_MAP_AK, latitude, longitude)
+    latitude, longitude = cal_offset(latitude, longitude)
+    return 'http://api.map.baidu.com/geocoder/v2/?ak=%s&location=%f,%f&output=json&pois=1' % (BAIDU_MAP_AK, latitude, longitude)
 
 
-def main(src_file_path, dst_file_path):
+def main(src_file_path, dst_file_path, log_file_path):
     ofile = open(dst_file_path, 'wb')
     writer = csv.writer(ofile)
+    logfile = open(log_file_path, 'w')
     with open(src_file_path, 'rb') as f:
         for line in f:
             longitude, latitude = line.strip().split(',')
             logging.info('fetching %s %s', latitude, longitude)
+            time.sleep(0.1)
             url = generate_url(latitude, longitude)
+            result = fetch_json_data(url)
+            if not result:
+                logfile.write("%s %s failed\n" % (latitude, longitude))
             writer.writerow([latitude + ' ' + longitude,
-                             fetch_json_data(url).encode('utf8')])
+                             result.encode('utf8')])
     ofile.close()
+    logfile.close()
 
 
 if __name__ == '__main__':
     import sys
     src_file_path = sys.argv[1]
     dst_file_path = sys.argv[2]
-    main(src_file_path, dst_file_path)
+    log_file_path = 'fetch_error.txt'
+    main(src_file_path, dst_file_path, log_file_path)
