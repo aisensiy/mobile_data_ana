f3fbc7a38fde93fb57734036575bf006eb7ab702
aisensiy <aisensiy@gmail.com>
2015-02-25 15:00:39 +0800

    split time counter func

--- gprs_count_statistic.py
+++ gprs_count_statistic.py
@@ -5,6 +5,13 @@
 统计每个用户每天以(半小时/一小时)粒度的请求数量
 """
 
+from csv import DictReader
+from collections import Counter
+from constants import log_header
+
+
+field_name = 'start_time'
+
 
 def split_by_hour(time_col):
     return time_col[6:10]
@@ -16,3 +23,11 @@ def split_by_halfhour(time_col):
     miniute = int(time_col[10:12])
     over_half = 1 if int(miniute) >= 30 else 0
     return date + ('%02d' % (hour * 2 + over_half))
+
+
+def gprs_statistic(fileobj, split_func):
+    reader = DictReader(fileobj, fieldnames=log_header)
+    counter = Counter()
+    for row in reader:
+        counter[split_func(row[field_name])] += 1
+    return counter
