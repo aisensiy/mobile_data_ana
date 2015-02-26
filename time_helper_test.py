ea99760d54751c1e965d6cfd7418456e58169640
aisensiy <aisensiy@gmail.com>
2015-02-26 19:36:52 +0800

    mv split time by 5 minute function to time helper module

---
+++ time_helper_test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import unittest
+import time_helper as th
+
+
+class ThTestCase(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
