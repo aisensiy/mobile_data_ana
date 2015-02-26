a3867e067fdb8dbfa81cda2ec3e57a0724b7c67c
aisensiy <aisensiy@gmail.com>
2015-02-26 19:20:02 +0800

    domain statistic by 5 minute

---
+++ url_split_statistic.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from csv import DictReader
+from csv import DictWriter
+import json
+from collections import Counter
+import logging
+from common import get_uid
+from url_helper import get_top_domain
+from constants import log_header
+
+logging.basicConfig(format='%(asctime)s-[%(levelname)s]: %(message)s',
+                    level=logging.DEBUG)
+
+
+def split_by_5_minute(time_col):
+    day_hour = time_col[6:10]
+    miniute = int(time_col[10:12]) / 5 * 5
+    return day_hour + ('%02d' % miniute)
+
+
+def encode_domain_counter(counter):
+    return "|".join(["%s:%d" % (domain, cnt)
+                     for domain, cnt in counter.most_common()])
+
+
+def domain_count_statistic(fileobj, split_func):
+    reader = DictReader(fileobj, fieldnames=log_header)
+    split_time_dict = {}
+    for row in reader:
+        time_split = split_by_5_minute(row['start_time'])
+        domain = get_top_domain(row['acce_url'])
+        if domain == '':
+            continue
+        if time_split not in split_time_dict:
+            split_time_dict[time_split] = Counter()
+        split_time_dict[time_split][domain] += 1
+    return [(time, encode_domain_counter(counter)) for time, counter in split_time_dict.items()]
+
+
+def save_to_csv(fileobj, uid, counter):
+    writer = DictWriter(fileobj, fieldnames=['uid', 'time', 'domain_count'])
+    for split_time, count in counter:
+        writer.writerow({'uid': uid, 'time': split_time, 'domain_count': count})
+
+
+def main():
+    import sys
+    import glob
+    import time
+    import os
+
+    start = time.time()
+
+    inputdir = sys.argv[1]
+    outputfile = sys.argv[2]
+    valid_user_file = sys.argv[3]
+
+    valid_users = set([line.strip() for line in open(valid_user_file)])
+    split_func = split_by_5_minute
+
+    csvfiles = glob.glob(os.path.join(inputdir, '*', '*.csv'))
+    n = len(csvfiles)
+
+    with open(outputfile, 'w') as outputfileobj:
+        for idx, csvfile in enumerate(csvfiles):
+            uid = get_uid(csvfile)
+            if uid not in valid_users:
+                continue
+            with open(csvfile, buffering=(2<<27)) as f:
+                counter = domain_count_statistic(f, split_func)
+            save_to_csv(outputfileobj, uid, counter)
+            logging.info('[%d/%d]' % (idx + 1, n))
+
+    logging.info('finish with time %s', str(time.time() - start))
+
+
+if __name__ == '__main__':
+    main()
