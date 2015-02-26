ff76a2aafed05d432a7a4eadcb2192900d722da7
aisensiy <aisensiy@gmail.com>
2015-02-26 09:43:47 +0800

    add get second domain

--- url_helper.py
+++ url_helper.py
@@ -7,10 +7,17 @@ url_pn = re.compile('^\S+//([^\/]+)/')
 ip_pn = re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
 
 
-def get_top_domain(url):
+def get_domain(url):
     url_match = url_pn.match(url)
     if url_match:
-        domain = url_match.group(1)
+        return url_match.group(1)
+    else:
+        return ''
+
+
+def get_top_domain(url):
+    domain = get_domain(url)
+    if domain:
         if ip_pn.match(domain):
             return domain
         splited_domain = domain.split('.')
@@ -24,3 +31,17 @@ def get_top_domain(url):
         return ''
 
 
+def get_second_domain(url):
+    domain = get_domain(url)
+    if domain:
+        if ip_pn.match(domain):
+            return domain
+        splited_domain = domain.split('.')
+        if len(splited_domain) >= 2 and \
+                splited_domain[-2] in ['com', 'net', 'info']:
+            level_domain = '.'.join(splited_domain[-4:])
+        else:
+            level_domain = '.'.join(splited_domain[-3:])
+        return level_domain
+    else:
+        return ''
