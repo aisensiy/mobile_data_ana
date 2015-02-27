b8016500fb53c7d5a6a426bb902cba5b5f268d24
aisensiy <aisensiy@gmail.com>
2015-02-27 17:12:06 +0800

    use get right domain for domain statistic

--- url_split_statistic.py
+++ url_split_statistic.py
@@ -6,7 +6,7 @@ from csv import DictWriter
 from collections import Counter
 import logging
 from common import get_uid
-from url_helper import get_top_domain
+from url_helper import generate_get_right_domain
 from constants import log_header
 from time_helper import split_by_5_minute
 
@@ -19,13 +19,13 @@ def encode_domain_counter(counter):
                      for domain, cnt in counter.most_common()])
 
 
-def domain_count_statistic(fileobj, split_func):
+def domain_count_statistic(fileobj, split_func, get_domain_func):
     reader = DictReader(fileobj, fieldnames=log_header)
     split_time_dict = {}
     for row in reader:
         time_split = split_by_5_minute(row['start_time'])
-        domain = get_top_domain(row['acce_url'])
-        if domain == '':
+        domain = get_domain_func(row['acce_url'])
+        if not domain:
             continue
         if time_split not in split_time_dict:
             split_time_dict[time_split] = Counter()
@@ -50,9 +50,17 @@ def main():
     inputdir = sys.argv[1]
     outputfile = sys.argv[2]
     valid_user_file = sys.argv[3]
+    topdomainfile = sys.argv[4]
+    seconddomainfile = sys.argv[5]
+    blacklistdomainfile = sys.argv[6]
+
+    topdomain_set = set([line.strip().split(',')[0] for line in open(topdomainfile)])
+    seconddomain_set = set([line.strip().split(',')[0] for line in open(seconddomainfile)])
+    blacklistdomain_set = set([line.strip().split(',')[0] for line in open(blacklistdomainfile)])
 
     valid_users = set([line.strip() for line in open(valid_user_file)])
     split_func = split_by_5_minute
+    get_domain_func = generate_get_right_domain(topdomain_set, seconddomain_set, blacklistdomain_set)
 
     csvfiles = glob.glob(os.path.join(inputdir, '*', '*.csv'))
     n = len(csvfiles)
@@ -62,8 +70,8 @@ def main():
             uid = get_uid(csvfile)
             if uid not in valid_users:
                 continue
-            with open(csvfile, buffering=(2<<27)) as f:
-                counter = domain_count_statistic(f, split_func)
+            with open(csvfile, buffering=(2 << 27)) as f:
+                counter = domain_count_statistic(f, split_func, get_domain_func)
             save_to_csv(outputfileobj, uid, counter)
             logging.info('[%d/%d]' % (idx + 1, n))
 
