8d93587896f0ed9c7d56b1c19d1c949ef0fd7bf6
aisensiy <aisensiy@gmail.com>
2015-02-28 20:21:03 +0800

    create table by sqlalchemy

--- import_to_db.py
+++ import_to_db.py
@@ -2,6 +2,7 @@
 # encoding: utf-8
 
 import sqlalchemy
+from sqlalchemy import MetaData, String, Integer, Table, Column
 import pandas as pd
 import logging
 
@@ -9,19 +10,35 @@ import logging
 logging.basicConfig(format='%(asctime)s-[%(levelname)s]: %(message)s',
                     level=logging.DEBUG)
 
+
 def import_app_domain_table(engine, df, tablename):
     pass
 
 
-def import_location_table(engine, df, tablename):
+def create_location_table(tablename, engine):
+    meta = MetaData(bind=engine)
+    Table(tablename, meta,
+          Column('id', Integer, primary_key=True, autoincrement=True),
+          Column('uid', String(16), nullable=False),
+          Column('start_time', String(14)),
+          Column('location', String(18)))
+    meta.create_all(engine)
+
+
+def import_location_table(engine, filepath, tablename, chunksize=10000):
     cols = ['uid', 'start_time', 'location']
-    df.columns = cols
-    import_to_db(engine, df, tablename)
+    df = pd.read_csv(filepath,
+                     names=cols,
+                     dtype={'uid': str, 'start_time': str},
+                     chunksize=chunksize)
+    for dataframe in df:
+        import_to_db(engine, dataframe, tablename)
+        logging.info('a chunk')
 
 
 def import_to_db(engine, df, tablename):
-    df = pd.read_csv('38210600.reqid.csv', header=False)
-    df.to_sql('test', engine)
+    df.to_sql(tablename, engine, index=False,
+              if_exists='append', chunksize=1000)
 
 
 if __name__ == '__main__':
@@ -33,13 +50,14 @@ if __name__ == '__main__':
     filepath = sys.argv[1]
     tablename = sys.argv[2]
     tabletype = sys.argv[3]
+    dbstring = sys.argv[4]
 
-    df = pd.read_csv(filepath, header=None)
-    engine = sqlalchemy.create_engine(
-        "mysql://root:000000@localhost/mobile_data_development")
+    # "mysql://root:000000@localhost/mobile_data_development"
+    engine = sqlalchemy.create_engine(dbstring)
 
     logging.info('import %s to %s', filepath, tablename)
     if tabletype == 'location':
-        import_location_table(engine, df, tablename)
+        create_location_table(tablename, engine)
+        import_location_table(engine, filepath, tablename)
 
     logging.info('finish with time %s', str(time.time() - start))
