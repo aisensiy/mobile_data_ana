91089265b3a6eaf0dfe8d796883501a126d0b7e9
aisensiy <aisensiy@gmail.com>
2015-03-05 19:12:10 +0800

    add gprs hour count import

--- import_to_db.py
+++ import_to_db.py
@@ -15,6 +15,31 @@ def import_app_domain_table(engine, df, tablename):
     pass
 
 
+def import_gprs_hour_stat_table(engine, filepath, tablename, chunksize=10000):
+    cols = ['uid', 'hour', 'count']
+    df = pd.read_csv(filepath,
+                     names=cols,
+                     dtype={'uid': str, 'hour': str},
+                     chunksize=chunksize)
+
+    for dataframe in df:
+        dataframe['day'] = dataframe.hour.map(lambda x: x[:2])
+        dataframe['hour'] = dataframe.hour.map(lambda x: x[-2:])
+        import_to_db(engine, dataframe, tablename)
+        logging.info('a chunk')
+
+
+def create_grps_hour_stat_table(tablename, engine):
+    meta = MetaData(bind=engine)
+    Table(tablename, meta,
+          Column('id', Integer, primary_key=True, autoincrement=True),
+          Column('uid', String(16), nullable=False),
+          Column('day', String(2)),
+          Column('hour', String(2)),
+          Column('count', Integer))
+    meta.create_all(engine)
+
+
 def create_location_table(tablename, engine):
     meta = MetaData(bind=engine)
     Table(tablename, meta,
@@ -59,5 +84,8 @@ if __name__ == '__main__':
     if tabletype == 'location':
         create_location_table(tablename, engine)
         import_location_table(engine, filepath, tablename)
+    elif tabletype == 'gprs_hour_count':
+        create_grps_hour_stat_table(tablename, engine)
+        import_gprs_hour_stat_table(engine, filepath, tablename)
 
     logging.info('finish with time %s', str(time.time() - start))
